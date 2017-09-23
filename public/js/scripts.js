65f2f3c91253746acf8cd196b2a550a8619e154f
Chris Scott <chris.scott718@gmail.com>
2017-09-23 11:59:35 -0700

    added scroll functionality to nav menu

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,5 +4,17 @@ $(function() {
     $("#navbarNav").toggleClass('show');
   }
 
+  function menuScroll(e) {
+    const target = e.target.dataset.scrollto;
+    $("html, body").delay(500).animate({
+      scrollTop: $(target).offset().top
+    }, {
+      duration: 500,
+      start: toggleNavigation()
+    });
+  }
+
   $(".navToggle").click(toggleNavigation);
+  $(".nav-link-lg" ).click(menuScroll);
+
 });
